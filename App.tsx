57603ae2ba2c6fef708550edd3657341c1e59f98
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-05-04 21:20:14 -0300

    setup: theme

--- App.tsx
+++ App.tsx
@@ -5,19 +5,19 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { NativeBaseProvider } from "native-base";
+import { Loading } from "@components/Loading";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  // const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  const fontsLoaded = false;
   return (
-    fontsLoaded && (
-      <NativeBaseProvider>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        <Text>Open up App.tsx to start working on your app!</Text>
-      </NativeBaseProvider>
-    )
+    <NativeBaseProvider>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Text></Text> : <Loading />}
+    </NativeBaseProvider>
   );
 }
