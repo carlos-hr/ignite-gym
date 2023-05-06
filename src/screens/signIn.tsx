2352ed9f6de286f5a6989bea9f87b449b11feb47
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-05-05 23:47:57 -0300

    feat: setup svg transformer and adding items to signin

--- src/screens/signIn.tsx
+++ src/screens/signIn.tsx
@@ -1,5 +1,6 @@
-import { VStack, Image } from "native-base";
+import { VStack, Image, Text, Center, Heading } from "native-base";
 import BackgroundImg from "@assets/background.png";
+import LogoSvg from "@assets/logo.svg";
 
 export function SignIn() {
   return (
@@ -10,6 +11,20 @@ export function SignIn() {
         resizeMode="contain"
         position="absolute"
       />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
+
+      <Center>
+        <Heading fontSize="xl" mb={6} fontFamily="heading" color="gray.100">
+          Acesse sua conta
+        </Heading>
+      </Center>
     </VStack>
   );
 }
