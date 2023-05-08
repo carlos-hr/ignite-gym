7d9eb0e3a1a0a2ab633b1d794dfaaed80a4fbf3f
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-05-08 10:25:06 -0300

    feat: adding components to signup page

---
+++ src/screens/signUp.tsx
@@ -0,0 +1,51 @@
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import BackgroundImg from "@assets/background.png";
+import LogoSvg from "@assets/logo.svg";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+export function SignUp() {
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bgColor="gray.700" px={10} pb={16}>
+        <Image
+          source={BackgroundImg}
+          alt="People training"
+          resizeMode="contain"
+          position="absolute"
+        />
+
+        <Center my={20}>
+          <LogoSvg />
+
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading fontSize="xl" mb={6} fontFamily="heading" color="gray.100">
+            Crie sua conta
+          </Heading>
+
+          <Input placeholder="Nome" />
+
+          <Input
+            placeholder="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" secureTextEntry />
+          <Input placeholder="Confirme a senha" secureTextEntry />
+
+          <Button title="Criar e acessar" />
+        </Center>
+
+        <Button mt={20} title="Voltar para o login" variant="outline" />
+      </VStack>
+    </ScrollView>
+  );
+}
