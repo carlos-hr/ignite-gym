a9f1d172aa3e6212b60aabba8ded47cde4409afc
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-05-08 10:24:43 -0300

    feat: creating signin page

--- src/screens/signIn.tsx
+++ src/screens/signIn.tsx
@@ -1,30 +1,53 @@
-import { VStack, Image, Text, Center, Heading } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bgColor="gray.700">
-      <Image
-        source={BackgroundImg}
-        alt="People training"
-        resizeMode="contain"
-        position="absolute"
-      />
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bgColor="gray.700" px={10} pb={16}>
+        <Image
+          source={BackgroundImg}
+          alt="People training"
+          resizeMode="contain"
+          position="absolute"
+        />
 
-      <Center my={24}>
-        <LogoSvg />
+        <Center my={24}>
+          <LogoSvg />
 
-        <Text color="gray.100" fontSize="sm">
-          Treine sua mente e o seu corpo
-        </Text>
-      </Center>
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
 
-      <Center>
-        <Heading fontSize="xl" mb={6} fontFamily="heading" color="gray.100">
-          Acesse sua conta
-        </Heading>
-      </Center>
-    </VStack>
+        <Center>
+          <Heading fontSize="xl" mb={6} fontFamily="heading" color="gray.100">
+            Acesse sua conta
+          </Heading>
+
+          <Input
+            placeholder="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" secureTextEntry />
+
+          <Button title="Acessar" />
+        </Center>
+
+        <Center mt={24}>
+          <Text fontSize="sm" fontFamily="body" mb={3} color="gray.100">
+            Ainda não tem acesso?
+          </Text>
+          <Button title="Criar conta" variant="outline" />
+        </Center>
+      </VStack>
+    </ScrollView>
   );
 }
