9fa27213aa76b3d7f1c9dccf7c3b40fe643fdd6d
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-05-15 22:12:03 -0300

    feat: creating route types and renaming folder

---
+++ src/routes/types.d.ts
@@ -0,0 +1,8 @@
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+export type AuthRoutes = {
+  signIn: undefined;
+  signUp: undefined;
+};
+
+// export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>;
