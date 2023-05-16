c2567c631f47443d25e38f24dfbd8d0028dc6935
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-05-16 19:24:15 -0300

    feat: creating page files

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,6 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { SignIn } from "@screens/signIn";
-import { SignUp } from "@screens/signUp";
+import { SignIn } from "@screens/SignIn";
+import { SignUp } from "@screens/SignUp";
 import { AuthRoutesType } from "./types";
 
 const { Navigator, Screen } = createNativeStackNavigator<AuthRoutesType>();
