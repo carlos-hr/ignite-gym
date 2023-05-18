3fc89f77df09953fa4c00f720ab393fec588fbea
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-05-18 20:38:07 -0300

    feat: adding logout icon

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -6,7 +6,7 @@ export function HomeHeader() {
     <HStack bgColor="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserAvatar alt="user profile image" size={16} mr={4} />
 
-      <VStack>
+      <VStack flex={1}>
         <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
@@ -15,6 +15,10 @@ export function HomeHeader() {
           User
         </Heading>
       </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
     </HStack>
   );
 }
