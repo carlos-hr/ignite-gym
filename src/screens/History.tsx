75cda5da07389f5e830b1da022693b91a912210c
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-05-24 18:20:27 -0300

    feat: adding mocked section list

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,12 +1,34 @@
 import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
-import { Center, Text, VStack } from "native-base";
+import { Center, Heading, Text, VStack, SectionList } from "native-base";
 
 export function History() {
+  const DATA = [
+    {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"],
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+    },
+  ];
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de exercícios" />
 
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {title}
+          </Heading>
+        )}
+        px={8}
+      />
       <HistoryCard />
     </VStack>
   );
