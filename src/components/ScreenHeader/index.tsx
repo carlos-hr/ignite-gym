8b04ce22e694661b78bf5f1308b7c421af7f8022
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-05-24 18:05:57 -0300

    feat: creating screen header

---
+++ src/components/ScreenHeader/index.tsx
@@ -0,0 +1,13 @@
+import { Center, Heading } from "native-base";
+import { ScreenHeaderProps } from "./types";
+
+export function ScreenHeader(props: ScreenHeaderProps) {
+  const { title } = props;
+  return (
+    <Center bg="gray.600" pb={6} pt={16}>
+      <Heading color="gray.100" fontSize="xl">
+        {title}
+      </Heading>
+    </Center>
+  );
+}
