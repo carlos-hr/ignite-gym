9e4bc5868fb00717fcf702048f69cd79f1fc45b3
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-05-29 11:56:37 -0300

    feat: accessing user images to set avatar

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,6 @@
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { Center, Text, VStack, FlatList, HStack, Heading } from "native-base";
+import { Text, VStack, FlatList, HStack, Heading } from "native-base";
 import { useState } from "react";
 import { WORKOUT_GROUPS } from "@constants/groups";
 import { ExerciseCard } from "@components/ExerciseCard";
