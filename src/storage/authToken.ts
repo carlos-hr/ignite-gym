57569e87c7ec38b9fd2b8ce046ef60996e9e31a7
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-06-20 15:06:45 -0300

    fix: refactor storage

--- src/storage/authToken.ts
+++ src/storage/authToken.ts
@@ -2,14 +2,26 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { AUTH_TOKEN_STORAGE } from '@storage/config';
 
-export async function storeAuthToken(token: string) {
-  await AsyncStorage.setItem(AUTH_TOKEN_STORAGE, token);
+interface AuthData {
+  token: string;
+  refresh_token: string;
+}
+
+export async function storeAuthToken(token: string, refresh_token: string) {
+  await AsyncStorage.setItem(
+    AUTH_TOKEN_STORAGE,
+    JSON.stringify({ token, refresh_token })
+  );
 }
 
 export async function getStoredAuthToken() {
-  const token = await AsyncStorage.getItem(AUTH_TOKEN_STORAGE);
+  const response = await AsyncStorage.getItem(AUTH_TOKEN_STORAGE);
 
-  return token;
+  const { token, refresh_token }: AuthData = response
+    ? JSON.parse(response)
+    : {};
+
+  return { token, refresh_token };
 }
 
 export async function deleteStoredAuthToken() {
