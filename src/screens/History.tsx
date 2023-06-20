8be517cee027c5367effd719c136abef1a5f3642
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-06-20 15:51:29 -0300

    feat: adding loading to history screen

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,4 +1,5 @@
 import { HistoryCard } from '@components/HistoryCard';
+import { Loading } from '@components/Loading';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryByDayDTO } from '@dtos/history';
 import { useError } from '@hooks/useError';
@@ -9,15 +10,19 @@ import { useCallback, useState } from 'react';
 
 export function History() {
   const [history, setHistory] = useState<HistoryByDayDTO[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const { showError } = useError();
 
   async function fetchHistory() {
     try {
+      setIsLoading(true);
       const { data } = await api.get('/history');
       setHistory(data);
     } catch (error) {
       showError(error, 'Não foi possível carregar o histórico');
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -31,32 +36,36 @@ export function History() {
     <VStack flex={1}>
       <ScreenHeader title="Histórico de exercícios" />
 
-      <SectionList
-        px={8}
-        sections={history}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <HistoryCard data={item} />}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={
-          history.length === 0 && { flex: 1, justifyContent: 'center' }
-        }
-        renderSectionHeader={({ section: { title } }) => (
-          <Heading
-            color="gray.200"
-            fontSize="md"
-            mt={10}
-            mb={3}
-            fontFamily="heading"
-          >
-            {title}
-          </Heading>
-        )}
-        ListEmptyComponent={() => (
-          <Text color="gray.100" textAlign="center">
-            Não há exercícios registrados ainda.
-          </Text>
-        )}
-      />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <SectionList
+          px={8}
+          sections={history}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <HistoryCard data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={
+            history.length === 0 && { flex: 1, justifyContent: 'center' }
+          }
+          renderSectionHeader={({ section: { title } }) => (
+            <Heading
+              color="gray.200"
+              fontSize="md"
+              mt={10}
+              mb={3}
+              fontFamily="heading"
+            >
+              {title}
+            </Heading>
+          )}
+          ListEmptyComponent={() => (
+            <Text color="gray.100" textAlign="center">
+              Não há exercícios registrados ainda.
+            </Text>
+          )}
+        />
+      )}
     </VStack>
   );
 }
