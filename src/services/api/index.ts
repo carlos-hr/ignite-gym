1f5b0ab8a9c7a66518ddb52fa26d9debbd05b57a
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-06-20 15:43:09 -0300

    feat: using refresh token

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,11 +1,14 @@
 import { AppError } from '@utils/appError';
-import { APIInstanceProps } from './types';
+import { APIInstanceProps, PromiseType } from './types';
 import axios from 'axios';
-import { getStoredAuthToken } from '@storage/authToken';
+import { getStoredAuthToken, storeAuthToken } from '@storage/authToken';
 
 export const api = axios.create({
   baseURL: 'http://192.168.2.112:3333',
 }) as APIInstanceProps;
+
+const failedQueue: PromiseType[] = [];
+let isRefreshing = false;
 
 api.registerInterceptTokenManager = (signOut) => {
   const interceptTokenManager = api.interceptors.response.use(
@@ -22,6 +25,59 @@ api.registerInterceptTokenManager = (signOut) => {
             signOut();
             return Promise.reject(error);
           }
+
+          const requestConfig = error.config;
+
+          if (isRefreshing) {
+            return new Promise((resolve, reject) => {
+              failedQueue.push({
+                onSuccess: (token) => {
+                  requestConfig.headers = { Authorization: `Bearer ${token}` };
+                  resolve(api(requestConfig));
+                },
+                onFailure: (error) => {
+                  reject(error);
+                },
+              });
+            });
+          }
+
+          isRefreshing = true;
+
+          return new Promise(async (resolve, reject) => {
+            try {
+              const { data } = await api.post('/sessions/refresh-token', {
+                refresh_token,
+              });
+
+              await storeAuthToken(data.token, data.refresh_token);
+
+              if (requestConfig.data) {
+                requestConfig.data = JSON.parse(requestConfig.data);
+              }
+
+              requestConfig.headers = { Authorization: `Bearer ${data.token}` };
+              api.defaults.headers.common[
+                'Authorization'
+              ] = `Bearer ${data.token}`;
+
+              failedQueue.forEach((req) => {
+                req.onSuccess(data.token);
+              });
+
+              resolve(api(requestConfig));
+            } catch (error: any) {
+              failedQueue.forEach((req) => {
+                req.onFailure(error);
+              });
+
+              signOut();
+              reject(error);
+            } finally {
+              isRefreshing = false;
+              failedQueue.splice(0, failedQueue.length);
+            }
+          });
         }
 
         signOut();
--- src/services/api/types.d.ts
+++ src/services/api/types.d.ts
@@ -1,7 +1,12 @@
-import { AxiosInstance } from 'axios';
+import { AxiosError, AxiosInstance } from 'axios';
 
 type SignOut = () => void;
 
 export interface APIInstanceProps extends AxiosInstance {
   registerInterceptTokenManager: (signOut: SignOut) => () => void;
+}
+
+export interface PromiseType {
+  onSuccess: (token: string) => void;
+  onFailure: (error: AxiosError) => void;
 }
